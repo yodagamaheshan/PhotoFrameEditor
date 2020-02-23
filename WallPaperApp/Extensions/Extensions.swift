bdb371108f667009034456d3dbb3b687c89386fe
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-23 10:52:53 +0530

    add app color enum

---
+++ WallPaperApp/Extensions/Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  Extensions.swift
+//  WallPaperApp
+//
+//  Created by Heshan Yodagama on 2/23/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    
+    enum AppColor {
+        case darkYellow
+        case lightYellow
+        case pink
+    }
+}
