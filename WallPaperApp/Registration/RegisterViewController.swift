1bba9a1f1fd5429107f5fd4c2ff7e5030b226ffc
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-23 19:45:14 +0530

    add simple UI to test

--- WallPaperApp/Registration/RegisterViewController.swift
+++ WallPaperApp/Registration/RegisterViewController.swift
@@ -10,21 +10,20 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    }
+    @IBAction func signupButtonPressed(_ sender: Any) {
+        
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
