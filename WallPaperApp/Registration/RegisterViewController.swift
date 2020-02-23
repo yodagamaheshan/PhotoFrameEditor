5f5f3864f1d6c5754eec7af8c02d445f94b094b6
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-23 22:09:08 +0530

    add validation

--- WallPaperApp/Registration/RegisterViewController.swift
+++ WallPaperApp/Registration/RegisterViewController.swift
@@ -15,19 +15,46 @@ class RegisterViewController: UIViewController {
     @IBOutlet weak var emailTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
-    
+    static func create() -> RegisterViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle(for: self))
+        let viewController = storyboard.instantiateViewController(withIdentifier: String(describing: RegisterViewController.self)) as? RegisterViewController
+        return viewController!
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     @IBAction func signupButtonPressed(_ sender: Any) {
+        let name = firstNameTF.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let email = emailTF.text?.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTF.text?.trimmingCharacters(in: .whitespacesAndNewlines)
         
-        Auth.auth().createUser(withEmail: emailTF.text!, password: passwordTF.text!) { authResult, error in
-            debugPrint(authResult)
+        if isValied(name: name, email: email, and: password){
+            Auth.auth().createUser(withEmail: email!, password: password!) { authResult, error in
+                debugPrint(authResult)
+            }
+        } else {
+            return
         }
         
     }
     
+    func isValied(name: String?,email: String?,and password: String?) -> Bool{
+        if name?.isEmpty ?? true{
+            self.showAllert(and: "Enter Name")
+            return false
+        }else if email?.isEmpty ?? true{
+            self.showAllert(and: "Enter Email")
+            return false
+        }else if password?.isEmpty ?? true{
+            self.showAllert(and: "Enter Password")
+            return false
+        }else if (password?.count ?? 0 < 6){
+            self.showAllert(and: "Passwor must be greater then 6 digit")
+            return false
+        }
+        return true
+    }
 
 }
