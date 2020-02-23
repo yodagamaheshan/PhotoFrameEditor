e1fc103c5f9b4485d966602899682679dd55bd3a
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-23 12:38:21 +0530

    add base file setup

---
+++ WallPaperApp/Registration/Login/Home/Frames/FramesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FramesViewController.swift
+//  WallPaperApp
+//
+//  Created by Heshan Yodagama on 2/23/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import UIKit
+
+class FramesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
