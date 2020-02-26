93c8ee6a50eb4b7dc0ef952651bee0b157c99bfe
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-26 20:20:04 +0530

    add on link click

--- WallPaperApp/SplashScreen/Registration/Login/Home/HomeViewController.swift
+++ WallPaperApp/SplashScreen/Registration/Login/Home/HomeViewController.swift
@@ -27,4 +27,11 @@ class HomeViewController: UIViewController {
             button.styleWithRoundCorner(with: UIColor.getAppColor(color: .darkBlackBlue) ?? UIColor.black, filling: UIColor.getAppColor(color: .darkGray) ?? UIColor.gray)
         }
     }
+    
+    @IBAction func onLinkPressed(_ sender: Any) {
+        if let url = URL(string: "https://yourphotoonit.com/pages/signup-wallpaper") {
+            UIApplication.shared.open(url)
+        }
+    }
+    
 }
