8be39f628e88f685aecb9b79828055b624d071d4
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-26 18:53:37 +0530

    add frame image collection view cell

---
+++ WallPaperApp/SplashScreen/Registration/Login/Home/Frames/FrameViewCell/FrameCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  FrameCollectionViewCell.swift
+//  WallPaperApp
+//
+//  Created by Heshan Yodagama on 2/26/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import UIKit
+
+class FrameCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var frameImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
