8cd6bc3d24bd893194464ef68edef527f195c8fc
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-27 09:28:01 +0530

    font preview collectionViewCell

---
+++ WallPaperApp/SplashScreen/Registration/Login/Home/Frames/EditPhoto/TextInput/FontPreviewCollectionViewCell/FontPreviewCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FontPreviewCollectionViewCell.swift
+//  WallPaperApp
+//
+//  Created by Heshan Yodagama on 2/27/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import UIKit
+
+class FontPreviewCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func setFont(font: UIFont){
+        nameLabel.font = font
+    }
+
+}
