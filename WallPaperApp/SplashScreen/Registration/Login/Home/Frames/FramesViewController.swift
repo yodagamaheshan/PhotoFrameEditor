5124745d20692364fc1474e28513aa8e29733ffc
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-29 09:51:49 +0530

    add getData method bug fix in collection cell size

--- WallPaperApp/SplashScreen/Registration/Login/Home/Frames/FramesViewController.swift
+++ WallPaperApp/SplashScreen/Registration/Login/Home/Frames/FramesViewController.swift
@@ -38,17 +38,49 @@ class FramesViewController: UIViewController {
     func listenForFireBaseData(){
         ref = Database.database().reference().child("Birthday_Frames")
               ref.observe(.value) { (dataSnapshot) in
+                
                 let allFrames = dataSnapshot.value as! [String : Any]
                 self.frameCollection.removeAll()
                 for frame in allFrames{
                     let image = frame.value as! [String:String]
-                    self.frameCollection.append( Frame(frameName: frame.key, imageURL: image["image"]!, unlock: image["unlock"]! == "yes" ? true:false))
+                    self.frameCollection.append( Frame(frameName: frame.key, imageURL: image["image"]!, unlock: image["unlock"]! == "yes" ? true:false, isRequestSent: false, tappedOnce: false))
                 }
-                self.frameCollectionView.reloadData()
+                
+                // download images
+                self.getData(frames: self.frameCollection) { (framesWithImages) in
+                    self.frameCollection = framesWithImages
+                   DispatchQueue.main.async {
+                      self.frameCollectionView.reloadData()
+                    }
+                    
+                }
             }
     }
     
+    
+    func getData(frames: [Frame],completion: @escaping ([Frame])-> Void ){
+        var framesToDownload = frames
+        var downloadedCount = 0
+        
+        for index in framesToDownload.indices{
+            let frame = framesToDownload[index]
+            URLSession.shared.dataTask(with: URL(string: frame.imageURL)!) { (data, response, error) in
+                downloadedCount += 1
+                
+                if data != nil{
+                    framesToDownload[index].image = UIImage(data: data!)
+                }else{
+                    framesToDownload[index].image = nil
+                }
+                if downloadedCount == framesToDownload.count{
+                    completion(framesToDownload)
+                }
+            }.resume()
+        }
+        
+    }
 }
+
 extension FramesViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return frameCollection.count
@@ -57,6 +89,7 @@ extension FramesViewController: UICollectionViewDataSource{
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: String(describing: FrameCollectionViewCell.self), for: indexPath) as! FrameCollectionViewCell
         cell.setPhotoFrame(frame: frameCollection[indexPath.row])
+        cell.setViewsForData(frame: frameCollection[indexPath.row])
         return cell
     }
     
@@ -88,69 +121,3 @@ extension FramesViewController: UICollectionViewDelegateFlowLayout{
 
 }
 
-extension FramesViewController{
-    
-    func downloadAllImages(_ urls: [URL], completion: @escaping ([UIImage]) -> Void) {
-        DispatchQueue.global(qos: .utility).async {
-            let group = DispatchGroup()
-            let semaphore = DispatchSemaphore(value: 4)
-            var imageDictionary: [URL: UIImage] = [:]
-
-            // download the images
-
-            for url in urls {
-                group.enter()
-                semaphore.wait()
-
-                self.download(url) { result in
-                    defer {
-                        semaphore.signal()
-                        group.leave()
-                    }
-
-                    switch result {
-                    case .failure(let error):
-                        print(error)
-
-                    case .success(let image):
-                        DispatchQueue.main.async {
-                            imageDictionary[url] = image
-                        }
-                    }
-                }
-            }
-
-            // now sort the results
-
-            group.notify(queue: .main) {
-                completion(urls.compactMap { imageDictionary[$0] })
-            }
-        }
-    }
-    
-    enum DownloadError: Error {
-        case notImage
-        case invalidStatusCode(URLResponse)
-    }
-
-    func download(_ url: URL, completion: @escaping (Result<UIImage, Error>) -> Void) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, let response = response as? HTTPURLResponse, error == nil else {
-                completion(.failure(error!))
-                return
-            }
-
-            guard 200..<300 ~= response.statusCode else {
-                completion(.failure(DownloadError.invalidStatusCode(response)))
-                return
-            }
-
-            guard let image = UIImage(data: data) else {
-                completion(.failure(DownloadError.notImage))
-                return
-            }
-
-            completion(.success(image))
-        }
-    }
-}
