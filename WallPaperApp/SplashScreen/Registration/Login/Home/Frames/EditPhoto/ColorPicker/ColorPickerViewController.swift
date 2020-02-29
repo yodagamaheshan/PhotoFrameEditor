bbc54a4ccaef2f0831d8d65198b2244998adfacd
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-29 11:23:36 +0530

    add ColorPickerViewController

---
+++ WallPaperApp/SplashScreen/Registration/Login/Home/Frames/EditPhoto/ColorPicker/ColorPickerViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ColorPickerViewController.swift
+//  WallPaperApp
+//
+//  Created by Heshan Yodagama on 2/29/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import UIKit
+
+class ColorPickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
