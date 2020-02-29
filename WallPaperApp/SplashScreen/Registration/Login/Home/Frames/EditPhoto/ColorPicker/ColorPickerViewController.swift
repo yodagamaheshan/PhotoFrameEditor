79d89a13f7822a9cbb78c4d558a86e74f8f33df5
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-29 12:13:07 +0530

    ColorPickerViewControllerDelegate

--- WallPaperApp/SplashScreen/Registration/Login/Home/Frames/EditPhoto/ColorPicker/ColorPickerViewController.swift
+++ WallPaperApp/SplashScreen/Registration/Login/Home/Frames/EditPhoto/ColorPicker/ColorPickerViewController.swift
@@ -9,18 +9,21 @@
 import UIKit
 import WDColorPicker
 
+protocol ColorPickerViewControllerDelegate {
+    func didSelectColor( color: UIColor)
+}
+
 class ColorPickerViewController: UIViewController, WDColorPickerViewDelegate {
     
     @IBOutlet weak var colorPicker: WDColorPickerView!
-    
+    var delegate: ColorPickerViewControllerDelegate?
+    var initialColor: UIColor?
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         self.colorPicker.delegate = self
-        self.colorPicker.layer.backgroundColor = UIColor.black.cgColor
     }
     
     func colorChanged(colorPicker: WDColorPickerView, color: UIColor) {
-        self.view.backgroundColor = color
+        delegate?.didSelectColor(color: color)
     }
 }
